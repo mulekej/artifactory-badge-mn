b0299ff6d62cbbe2ba34e6fdb72ec40984f6d70f
mulekej <ejmulek@gmail.com>
2018-07-30 22:12:39 -0500

    convert BadgeGenerator to groovy

--- src/main/groovy/artifactory/badge/mn/generator/BadgeGenerator.groovy
+++ src/main/groovy/artifactory/badge/mn/generator/BadgeGenerator.groovy
@@ -1,9 +1,6 @@
 package artifactory.badge.mn.generator
 
-import java.awt.Color
-import java.awt.Font
-import java.awt.Graphics2D
-import java.awt.RenderingHints
+import java.awt.*
 import java.awt.image.BufferedImage
 
 class BadgeGenerator {
@@ -23,16 +20,40 @@ class BadgeGenerator {
 
         graphics.renderingHints[RenderingHints.KEY_ANTIALIASING] = RenderingHints.VALUE_ANTIALIAS_ON
 
-        allignFont(graphics, (left + right), MAX_HEIGHT, TEXT_MARGIN)
+        alignFont(graphics, (left + right), MAX_HEIGHT, TEXT_MARGIN)
 
-        final def leftWidth =
-        final def rightWidth = 
+        final def leftWidth = graphics.fontMetrics.stringWidth(left)
+        final def rightWidth = graphics.fontMetrics.stringWidth(right)
 
-        image
+        graphics.paint = GRAY
+        graphics.fillRoundRect(0, 0, ROUNDED_ARC * 2, MAX_HEIGHT, ROUNDED_ARC, ROUNDED_ARC)
+        graphics.fillRect(ROUNDED_ARC, 0, leftWidth + (2 * TEXT_MARGIN) - ROUNDED_ARC, MAX_HEIGHT)
+
+        if (success) {
+            graphics.paint = GREEN
+        } else {
+            graphics.paint = RED
+        }
+
+        graphics.fillRoundRect(leftWidth + (2 * TEXT_MARGIN), 0, rightWidth + (2 * TEXT_MARGIN), MAX_HEIGHT, ROUNDED_ARC, ROUNDED_ARC)
+        graphics.fillRect(leftWidth + (2 * TEXT_MARGIN), 0, ROUNDED_ARC, MAX_HEIGHT)
+
+        //draw text
+        def lineMetrics = graphics.font.getLineMetrics(left, graphics.getFontRenderContext())
+
+        int y = (int)lineMetrics.getAscent() + TEXT_MARGIN
+        graphics.setPaint(Color.WHITE)
+        graphics.drawString(left, TEXT_MARGIN, y)
+        graphics.drawString(right, leftWidth + (3 * TEXT_MARGIN), y)
+
+        graphics.dispose()
+        int imageWidth = leftWidth + rightWidth + (4 * TEXT_MARGIN)
+        def result = image.getSubimage(0, 0, imageWidth < MAX_WIDTH ? imageWidth : MAX_WIDTH, MAX_HEIGHT)
+        result
     }
 
-    private static void allignFont(Graphics2D graphics, String text, int height, int margin) {
-        final def font = graphics.getFont()
+    private static void alignFont(Graphics2D graphics, String text, int height, int margin) {
+        final def font = graphics.font
         final def diff = (height - (2 * margin)) / font.getLineMetrics(text, graphics.getFontRenderContext()).getHeight()
 
         graphics.setFont(font.deriveFont(font.getSize() * diff))
