37bff9a4a4b3a98cff8119fe824bde6a5b8df2ed
mulekej <ejmulek@gmail.com>
2018-08-02 18:35:41 -0500

    move get back to api to fix fallback

--- src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryClient.groovy
+++ src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryClient.groovy
@@ -1,12 +1,8 @@
 package artifactory.badge.mn.artifactory
 
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.QueryValue
+
 import io.micronaut.http.client.Client
 
 @Client('http://${artifactory.host}:${artifactory.port}')
-interface ArtifactoryClient {
-
-    @Get(uri = "/artifactory/api/search/latestVersion?a={artifactId}&g={groupId}&repos={repo}")
-    String findVersion(@QueryValue("g") String groupId, @QueryValue("a") String artifactId, @QueryValue("repos") String repo)
+interface ArtifactoryClient extends ArtifactoryApi {
 }
--- src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryFallback.groovy
+++ src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryFallback.groovy
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.QueryValue
 import io.micronaut.retry.annotation.Fallback
 
 @Fallback
-class ArtifactoryFallback implements ArtifactoryClient{
+class ArtifactoryFallback implements ArtifactoryApi {
 
     @Override
     String findVersion(@QueryValue("g") String groupId, @QueryValue("a") String artifactId, @QueryValue("repos") String repo) {
