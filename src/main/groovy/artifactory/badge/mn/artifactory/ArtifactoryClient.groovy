452e31ac55550aef57199b6c5afcc21ebe3a7aed
Eric Mulek <eric.mulek@surescripts.com>
2018-08-02 16:04:35 -0500

    remove hard coded url

--- src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryClient.groovy
+++ src/main/groovy/artifactory/badge/mn/artifactory/ArtifactoryClient.groovy
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
 import io.micronaut.http.client.Client
 
-@Client("http://artifactory.surescripts-dev.qa:8080")
+@Client('http://${artifactory.host}:${artifactory.port}')
 interface ArtifactoryClient {
 
     @Get(uri = "/artifactory/api/search/latestVersion?a={artifactId}&g={groupId}&repos={repo}")
